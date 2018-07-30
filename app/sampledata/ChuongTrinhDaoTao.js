5ce87e27f601f9a04b9fa806a8969d567b53c618
Dong-Nhan <ddnn97@hotmail.com>
2018-07-30 22:01:04 +0700

    Update: khung điền dữ liệu cho sample data

--- app/sampledata/ChuongTrinhDaoTao.js
+++ app/sampledata/ChuongTrinhDaoTao.js
@@ -1,2 +1,59 @@
 //dữ liệu mẫu là mảng các JS object
 //theo thiết kế: https://docs.google.com/spreadsheets/d/1iER7kPLNYPeJ6zvwZ2w_YrTR4o-7ncuyLAIu8O8RI1Y/edit#gid=0
+exports.ChuongTrinhDaoTao = [{
+    tenChuongTrinhDaoTao: "",
+    trinhDoDaoTao: "",
+    hinhThucDaoTao: "",
+    khoaTuyen: "",
+    mucTieuDaoTao: {
+        mucTieuChung: "",
+        mucTieuCuThe: "",
+        coHoiNgheNghiep: ""
+    },
+    thoiGianDaoTao: "",
+    khoiLuongTinChi: "",
+    doiTuongTuyenSinh: "",
+    quyTrinhDaoTao: "",
+    cauTrucChuongTrinh: [{
+        khoiKienThuc: "",
+        chiTietSoTinChi: {
+            soTinChiBatBuoc: "",
+            soTinChiTuChon: "",
+            soTinChiTuChonTuDo: ""
+        },
+        soTinChiTongCong: "",
+        ghiChu: ""
+    }],
+    noiDungChuongTrinh:{
+        kienThucGiaoDucDaiCuong: {
+            ten: "",
+            khoiKienThuc: [{
+                ten: "",
+                dsHocPhan: []
+            }]
+        },
+        kienThucGiaoDucChuyenNghiep: {
+            ten: "",
+            kienThucCoSoNganh: {
+                ten: "",
+                dsHocPhan: []
+            },
+            kienThucChuyenNganh:{
+                ten: "",
+                dsLoai: [{
+                    ten: "",
+                    soHocPhanTichLuyMin: "",
+                    soTinChiTichLuyMin: "",
+                    dsHocPhan: []
+                }]
+            },
+            kienThucTotNghiep: {
+                ten: "",
+                soTinChiTichLuyMin: "",
+                dsHocPhan: []
+            }
+        }
+    },
+    dieuKienTotNghiep: "",
+    filePdfGoc: ""
+}]
--- app/sampledata/HocPhan.js
+++ app/sampledata/HocPhan.js
@@ -1,2 +1,14 @@
 //dữ liệu mẫu là mảng các JS object
 //theo thiết kế: https://docs.google.com/spreadsheets/d/1iER7kPLNYPeJ6zvwZ2w_YrTR4o-7ncuyLAIu8O8RI1Y/edit#gid=0
+exports.HocPhan = [{
+    maHocPhan: "",
+    tenHocPhan: "",
+    soTinChi: "",
+    loaiHocPhan: "",
+    nhomHocPhan: "",
+    soTietLyThuyet: "",
+    soTietThucHanh: "",
+    soTietBaiTap: "",
+    hocPhanTienQuyet: "",
+    hocPhanDeNghiHocTruoc: ""
+}]
