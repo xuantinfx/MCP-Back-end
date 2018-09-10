16919afe786cdce88a89f1a06a958c28f70f86fb
Xuan Tin <xuantinfx@gmail.com>
2018-09-10 10:46:07 +0700

    feature check-if-login

--- app/routes/routesWithAuth/users.js
+++ app/routes/routesWithAuth/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const viewProfile = require('../../controller/user/viewProfile')
+const checkIfLogin = require('../../controller/user/checkIfLogin')
 
 // Register New User
 router.get('/view-profile', viewProfile);
+router.get('/check-if-login', checkIfLogin);
 
 module.exports = router;
